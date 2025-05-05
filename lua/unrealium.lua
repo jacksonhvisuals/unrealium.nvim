1d2368b1fedef738a2fddb9960eb12e71610da13
Jackson Hayes <asecretgate@gmail.com>
2025-05-04 19:06:25 -0700

    Add initializing logic

---
+++ lua/unrealium.lua
@@ -0,0 +1,31 @@
+if 1 ~= vim.fn.has("nvim-0.10.0") then
+	vim.api.nvim_echo({ { "Unrealium requires at least nvim-0.10.0" } }, true, {})
+	return
+end
+
+-- Ensure that Unrealium hasn't already initialized
+if vim.g.unrealium_loaded == 1 then
+	return
+end
+vim.g.unrealium_loaded = 1
+
+-- Otherwise, register commands.
+vim.api.nvim_create_user_command("UGenProjectFiles", function(opts)
+	require("unrealium.commands").generateProjectFiles(opts)
+end, {})
+
+vim.api.nvim_create_user_command("UBuild", function(opts)
+	require("unrealium.commands").build(opts)
+end, {})
+
+vim.api.nvim_create_user_command("URun", function(opts)
+	require("unrealium.commands").run(opts)
+end, {})
+
+vim.api.nvim_create_user_command("UDebug", function(opts)
+	require("unrealium.commands").debug(opts)
+end, {})
+
+function setup(args)
+	print("Setting up Unrealium plugin.")
+end
