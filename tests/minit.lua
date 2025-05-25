a521cde554f034006a745c050b1dedbccdca399c
Jackson Hayes <asecretgate@gmail.com>
2025-05-24 19:01:25 -0700

    Add dependencies to minit

--- tests/minit.lua
+++ tests/minit.lua
@@ -6,6 +6,8 @@ load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/ma
 -- Setup lazy.nvim
 require("lazy.minit").setup({
 	spec = {
+		{ "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+		{ "radenling/vim-dispatch-neovim", dependencies = { "tpope/vim-dispatch" } },
 		{ dir = vim.uv.cwd() },
 	},
 })
